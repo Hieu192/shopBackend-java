ffd003d5e3ec1df417da97f25abfe0947a24fbb1
Hieu <21520235@gm.uit.edu.vn>
2025-04-01 20:35:13 +0700

    order detail

--- src/main/java/com/hieu/shopBackend/controllers/OrderDetailController.java
+++ src/main/java/com/hieu/shopBackend/controllers/OrderDetailController.java
@@ -1,4 +1,88 @@
 package com.hieu.shopBackend.controllers;
 
+import com.hieu.shopBackend.dtos.requests.order.OrderDetailRequest;
+import com.hieu.shopBackend.dtos.responses.ApiResponse;
+import com.hieu.shopBackend.dtos.responses.Order.OrderDetailResponse;
+import com.hieu.shopBackend.models.OrderDetail;
+import com.hieu.shopBackend.services.OrderDetailService;
+import com.hieu.shopBackend.utils.MessageKeys;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/order_details")
 public class OrderDetailController {
+    private final OrderDetailService orderDetailService;
+
+
+    @PostMapping("")
+    public ResponseEntity<?> createOrderDetail(
+            @Valid @RequestBody OrderDetailRequest orderDetailRequest
+    ) {
+        try {
+
+            OrderDetail newOrderDetail = orderDetailService.createOrderDetail(orderDetailRequest);
+            return ResponseEntity.ok(ApiResponse.builder().success(true)
+                    .message(MessageKeys.CREATE_ORDER_DETAILS_SUCCESS)
+                    .result(OrderDetailResponse.fromOrderDetail(newOrderDetail)).build());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(
+                    ApiResponse.builder()
+                            .message(MessageKeys.CREATE_ORDER_DETAILS_SUCCESS)
+                            .error(e.getMessage()).build()
+            );
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOrderDetail(@Valid @PathVariable("id") Long id) throws Exception {
+        OrderDetail orderDetail = orderDetailService.getOrderDetail(id);
+        return ResponseEntity.ok(ApiResponse.builder().success(true).result(OrderDetailResponse.fromOrderDetail(orderDetail)).build());
+        // return ResponseEntity.ok(OrderDetailResponse.fromOrderDetail(orderDetail));
+    }
+
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<?> getOrderDetails(@Valid @PathVariable("orderId") Long orderId) {
+        List<OrderDetailResponse> orderDetailResponses = orderDetailService.findByOrderId(orderId)
+                .stream()
+                .map(OrderDetailResponse::fromOrderDetail)
+                .toList();
+        return ResponseEntity.ok(ApiResponse.builder().success(true).result(orderDetailResponses).build());
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrderDetail(
+            @Valid @RequestBody OrderDetailRequest orderDetailRequest,
+            @PathVariable("id") Long id
+    ) {
+        try {
+            OrderDetail orderDetail = orderDetailService.updateOrderDetail(id, orderDetailRequest);
+            return ResponseEntity.ok(ApiResponse.builder().success(true).result(OrderDetailResponse.fromOrderDetail(orderDetail)).build());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ApiResponse.builder().error(e.getMessage()).build());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrderDetail(@Valid @PathVariable("id") Long id) {
+        try {
+            orderDetailService.deleteOrderDetail(id);
+            return ResponseEntity.ok().body(ApiResponse.builder().success(true)
+                    .message(MessageKeys.MESSAGE_DELETE_SUCCESS).build());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(ApiResponse.builder()
+                    .error(e.getMessage())
+                    .message(MessageKeys.MESSAGE_DELETE_SUCCESS).build());
+        }
+    }
 }
--- src/main/java/com/hieu/shopBackend/services/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/hieu/shopBackend/services/impl/OrderDetailServiceImpl.java
@@ -1,34 +1,81 @@
 package com.hieu.shopBackend.services.impl;
 
 import com.hieu.shopBackend.dtos.requests.order.OrderDetailRequest;
+import com.hieu.shopBackend.exceptions.payload.DataNotFoundException;
+import com.hieu.shopBackend.models.Order;
 import com.hieu.shopBackend.models.OrderDetail;
+import com.hieu.shopBackend.models.Product;
+import com.hieu.shopBackend.repositories.OrderDetailRepository;
+import com.hieu.shopBackend.repositories.OrderRepository;
+import com.hieu.shopBackend.repositories.ProductRepository;
 import com.hieu.shopBackend.services.OrderDetailService;
+import com.hieu.shopBackend.utils.MessageKeys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class OrderDetailServiceImpl implements OrderDetailService {
+
+    private final OrderDetailRepository orderDetailRepository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+
     @Override
     public OrderDetail createOrderDetail(OrderDetailRequest orderDetailRequest) throws Exception {
-        return null;
+        Order order = orderRepository.findById(orderDetailRequest.getOrderId())
+                .orElseThrow(() -> new DataNotFoundException(MessageKeys.NOT_FOUND + orderDetailRequest.getOrderId()));
+        Product product = productRepository.findById(orderDetailRequest.getProductId())
+                .orElseThrow(() -> new DataNotFoundException(MessageKeys.NOT_FOUND + orderDetailRequest.getProductId()));
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .order(order)
+                .product(product)
+                .numberOfProducts(orderDetailRequest.getNumberOfProducts())
+                .totalMoney(orderDetailRequest.getTotalMoney())
+                .price(orderDetailRequest.getPrice())
+                .color(orderDetailRequest.getColor())
+                .build();
+
+        return orderDetailRepository.save(orderDetail);
     }
 
     @Override
-    public OrderDetail getOrderDetail(Long id) throws Exception {
-        return null;
+    public OrderDetail getOrderDetail(Long id) throws DataNotFoundException {
+        return orderDetailRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException(MessageKeys.NOT_FOUND + id));
     }
 
     @Override
+    @Transactional
     public OrderDetail updateOrderDetail(Long id, OrderDetailRequest orderDetailRequest) {
-        return null;
+        OrderDetail existsOrderDetail = getOrderDetail(id);
+        Order existsOrder = orderRepository.findById(orderDetailRequest.getOrderId())
+                .orElseThrow(() -> new DataNotFoundException(MessageKeys.NOT_FOUND + orderDetailRequest.getOrderId()));
+        Product existsProduct = productRepository.findById(orderDetailRequest.getProductId())
+                .orElseThrow(() -> new DataNotFoundException(MessageKeys.NOT_FOUND + orderDetailRequest.getProductId()));
+
+        existsOrderDetail.setProduct(existsProduct);
+        existsOrderDetail.setNumberOfProducts(orderDetailRequest.getNumberOfProducts());
+        existsOrderDetail.setTotalMoney(orderDetailRequest.getTotalMoney());
+        existsOrderDetail.setPrice(orderDetailRequest.getPrice());
+        existsOrderDetail.setColor(orderDetailRequest.getColor());
+        existsOrderDetail.setId(id);
+        existsOrderDetail.setOrder(existsOrder);
+        return orderDetailRepository.save(existsOrderDetail);
     }
 
     @Override
+    @Transactional
     public void deleteOrderDetail(Long id) {
-
+        orderDetailRepository.deleteById(id);
     }
 
     @Override
     public List<OrderDetail> findByOrderId(Long orderId) {
-        return List.of();
+        return orderDetailRepository.findByOrderId(orderId);
     }
 }
